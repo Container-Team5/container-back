5df4167d9a74309e1618f46e8c9a7402d3992828
kyj11 <kyj466688@naver.com>
2023-03-24 01:12:55 +0900

    컨테이너 백엔드

---
+++ src/main/java/com/example/containerback/ContainerBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.containerback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContainerBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ContainerBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/containerback/ContainerBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.containerback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContainerBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
