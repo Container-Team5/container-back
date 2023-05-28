c88a624060c5aaceebae33d7af17c7625e78e9ad
kanginleeoficial <hil07031@gmail.com>
2023-05-28 16:41:40 +0900

    POST 문제 해결, 부피 구하는 문제 해결h

---
+++ src/main/java/com/example/containerback/controller/HomeController.java
@@ -0,0 +1,13 @@
+/*package com.example.containerback.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @RequestMapping(value = "/")
+    public String home() {
+        return "index";
+    }
+}*/
