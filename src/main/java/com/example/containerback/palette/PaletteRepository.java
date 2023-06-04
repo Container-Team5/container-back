df19c0d1a1f8d47553b5aa95eaba41d5d7e98590
kyj11 <kyj466688@naver.com>
2023-06-04 16:24:50 +0900

    테스트 전

--- src/main/java/com/example/containerback/palette/PaletteRepository.java
+++ src/main/java/com/example/containerback/palette/PaletteRepository.java
@@ -1,5 +1,6 @@
 package com.example.containerback.palette;
 
+import com.example.containerback.PaletteCustomRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,7 +8,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
-public interface PaletteRepository extends JpaRepository<Palette, Long> {
+public interface PaletteRepository extends JpaRepository<Palette, Long>, PaletteCustomRepository {
     List<Palette> findAllByPaletteId(Long paletteId);
     List<Palette> findByPaletteNameContains(String pName);
     List<Palette> findAllByDeadLineGreaterThanEqualAndDeadLineLessThanEqual(LocalDateTime DeadLineFrom, LocalDateTime DeadLineTo);
